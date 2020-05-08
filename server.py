a68f2ed0f2e0fab02ed53041725e591d7d573e0d
Jina Zhu <jinazhu87@gmail.com>
2020-05-08 14:28:42 -0700

    added sarah's changes

--- server.py
+++ server.py
@@ -2,6 +2,58 @@ from flask import Flask, session, jsonify, request
 from model import connect_to_db, db, User, Ingredient, Cocktail
 from model_helper import add_user
 
+app = Flask(__name__)
+app.secret_key = 'TEMP'
+
+
+@app.route('/register', methods=['POST'])
+def register_form():
+    """User register form"""
+
+    user_input = request.get_json()
+
+    user = User.query.filter(User.email == user_input['email']).first()
+
+    if user:
+        return 'User account already exists'
+
+    new_user = add_user(user_input)
+
+    session['user'] = new_user.user_id
+
+    response = {'user_name': new_user.first_name,
+                'message': 'Successfully registered!'}
+
+    return jsonify(response)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    """User login"""
+
+    user_login = request.get_json()
+
+    user = User.query.filter(User.email == user_login['email']).first()
+
+    if not (user) or (user.password != user_login['password']):
+        return 'The user or password information is incorrect'
+
+    session['user'] = user.user_id
+
+    response = {'user_name': user.first_name,
+                'message': 'Successfully logged in!'}
+
+    return jsonify(response)
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    """User logout"""
+
+    del session['user']
+
+    return ('', 204)
+
 
 @app.route('/save', method=["POST"])
 def user_saved_recipes():
@@ -44,58 +96,6 @@ def display_saved_cocktails():
                                       'img_url': cocktail.img_url, 'ingredients': cocktail.ing_name})
 
     return jsonify(saved_cocktail_detail)
-
-
-app = Flask(__name__)
-app.secret_key = 'TEMP'
-
-@app.route('/register', methods=['POST'])
-def register_form():
-    """User register form"""
-
-    user_input = request.get_json()
-
-    user = User.query.filter(User.email == user_input['email']).first()
-
-    if user:
-        return 'User account already exists'
-    
-    new_user = add_user(user_input)
-
-    session['user'] = new_user.user_id
-
-    response = {'user_name': new_user.first_name,
-                'message': 'Successfully registered!'}
-
-    return jsonify(response)
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    """User login"""
-
-    user_login = request.get_json()
-
-    user = User.query.filter(User.email == user_login['email']).first()
-
-    if not (user) or (user.password != user_login['password']):
-        return 'The user or password information is incorrect'
-    
-    session['user'] = user.user_id
-
-    response = {'user_name': user.first_name,
-                'message': 'Successfully logged in!'}
-
-    return jsonify(response)
-
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    """User logout"""
-
-    del session['user']
-
-    return ('', 204)
 
 
 if __name__ == '__main__':
