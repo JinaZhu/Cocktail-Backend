1ba8c01aa3d1d6d4dc5ea670f552955afb69e0e3
Kimberly Chuc <kimmchuc@gmail.com>
2020-05-08 11:25:10 -1000

    updating server.py with routes

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask, session, jsonify, request
+import requests
 from model import connect_to_db, db, User, Ingredient, Cocktail
 
+res = requests.get('https://www.thecocktaildb.com/api/json/v1/9973533/search.php?s=moscow_mule')
+
+cocktail_results = res.json()
+
+print(cocktail_results)
 
 app = Flask(__name__)
 app.secret_key = 'TEMP'
