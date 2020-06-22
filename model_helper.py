aa430455992b7ebb89cc35a858c05958a5fafda8
Sarah Tao <satao.94@gmail.com>
2020-06-22 15:38:09 -0700

    hash passwords and save to db

--- model_helper.py
+++ model_helper.py
@@ -1,14 +1,14 @@
 from sqlalchemy import func
 from model import connect_to_db, db, User, Ingredient, Cocktail
 
-def add_user(user_input):
+def add_user(user_input, pw_hash):
     """Save user information into database"""
 
     new_user = User(
                 email=user_input['email'],
                 first_name=user_input['fname'],
                 last_name=user_input['lname'],
-                password=user_input['password']
+                password=pw_hash
                 )
     
     db.session.add(new_user)
